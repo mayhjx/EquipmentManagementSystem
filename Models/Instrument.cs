4f404610651a96b696d9820b124f57385e38d7da
mayangjie <mayangjie@kingmen.com.cn>
2020-04-15 10:29:25 +0800

    修改字段名

--- Models/Instrument.cs
+++ Models/Instrument.cs
@@ -8,97 +8,98 @@ namespace EquipmentManagementSystem.Models
     public class Instrument
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        [Display(Name="编号")]
+        [Display(Name = "编号")]
         [RegularExpression(@"^[A-Z]+-[A-Z0-9]+$")]
         public string ID { get; set; }
-        
-        [Display(Name="创建时间")]
+
+        [Display(Name = "创建时间")]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public DateTime CreatedDate { get; set; }
 
         // SQLite 有bug
-        [Display(Name="修改时间")]
+        [Display(Name = "修改时间")]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public DateTime ModifiedDate { get; set; }
 
         [Required]
-        [Display(Name="平台")]
-        [StringLength(50, MinimumLength=1)]
+        [Display(Name = "平台")]
+        [StringLength(50, MinimumLength = 1)]
         public string Platform { get; set; }
 
         [Required]
-        [Display(Name="名称")]
-        [StringLength(50, MinimumLength=1)]
+        [Display(Name = "名称")]
+        [StringLength(50, MinimumLength = 1)]
         public string Name { get; set; }
-        
-        [Display(Name="启用日期")]
+
+        [Display(Name = "启用日期")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString="{0:yyyy-MM-dd}", ApplyFormatInEditMode=true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime StartUsingDate { get; set; }
 
-        [Range(1,5)]
-        [Display(Name="校准周期（年）")]
+        [Range(1, 5)]
+        [Display(Name = "校准周期（年）")]
         public int CalibrationCycle { get; set; }
 
-        [Display(Name="计量特性")]
-        [StringLength(10,MinimumLength=1)]
+        [Display(Name = "计量特性")]
+        [StringLength(10, MinimumLength = 1)]
         public string MetrologicalCharacteristics { get; set; }
 
-        [Display(Name="状态")]
+        [Display(Name = "状态")]
         public Status Status { get; set; }
 
         [Required]
-        [Display(Name="位置")]
-        [StringLength(50,MinimumLength=1)]
+        [Display(Name = "存放位置")]
+        [StringLength(50, MinimumLength = 1)]
         public string Location { get; set; }
 
-        [Required]      
-        [Display(Name="负责人")]
-        [StringLength(10,MinimumLength=1)]
+        [Required]
+        [Display(Name = "负责人")]
+        [StringLength(10, MinimumLength = 1)]
         public string Principal { get; set; }
 
-        [Display(Name="备注")]
+        [Display(Name = "备注")]
         [StringLength(999)]
         public string Remark { get; set; }
 
-        [Display(Name="新系统编号")]
+        [Display(Name = "新系统编号")]
         [StringLength(10)]
         public string NewSystemCode { get; set; }
 
-        [Display(Name="设备电脑")]
+        [Display(Name = "设备电脑")]
         public Computer computer { get; set; }
 
-        [Display(Name="资产信息")]
+        [Display(Name = "资产信息")]
         public Assert assert { get; set; }
 
-        [Display(Name="所属项目组")]
+        [Display(Name = "所属项目组")]
         public string ProjectTeamName { get; set; }
         public ProjectTeam projectTeam { get; set; }
 
-        [Display(Name="检测项目")]
+        [Display(Name = "检测项目")]
         public ICollection<Project> projects { get; set; }
 
-        [Display(Name="校准信息")]
+        [Display(Name = "校准信息")]
         public ICollection<Calibration> calibrations { get; set; }
 
-        [Display(Name="部件信息")]
+        [Display(Name = "部件信息")]
         public ICollection<Component> components { get; set; }
 
-        [Display(Name="故障信息")]
+        [Display(Name = "故障信息")]
         public ICollection<Malfunction> malfunctions { get; set; }
     }
 
-    public enum Status {
-        
-        [Display(Name="正常")]
+    public enum Status
+    {
+
+        [Display(Name = "正常")]
         Using,
-        [Display(Name="故障")]
+        [Display(Name = "故障")]
         Malfunction,
         [Display(Name = "停用")]
         StopUsing,
         [Display(Name = "调拨")]
         Allocate,
-        [Display(Name="报废")]
+        [Display(Name = "报废")]
         Scrapped
     }
 }
