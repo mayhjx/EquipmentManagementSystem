dc4883bb1a96f246d14e28d8b499a66469f1674f
mayangjie <mayangjie@kingmen.com.cn>
2020-04-15 10:10:10 +0800

    首页增加快到期设备提醒

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -25,17 +25,22 @@ namespace EquipmentManagementSystem.Pages
 
         public IList<Instrument> Instruments { get; set; }
 
+        // 快到期设备
+        public IList<Instrument> InstrumentOfExpire { get; set; }
+
         public void OnGet()
         {
-            var instruments = from m in _context.Instruments
-                              .Include(m => m.calibrations)
-                              .AsEnumerable()
-                              where m.calibrations.Last().Date != DateTime.Parse("0001-01-01")
-                              let remainDay = m.calibrations.Last().Date.AddYears(m.CalibrationCycle) - DateTime.Today
-                              where remainDay.Days < 30
-                              select m;
 
-            Instruments = instruments.ToList();                
+            InstrumentOfExpire = (from m in _context.Instruments
+                                .Include(m => m.calibrations)
+                                .AsEnumerable()
+                                where m.calibrations.Last().Date != DateTime.Parse("0001-01-01")
+                                let remainDay = m.calibrations.Last().Date.AddYears(m.CalibrationCycle) - DateTime.Today
+                                where remainDay.Days < 30 // 到期前30天内
+                                select m).ToList();
+
+            Instruments = (from m in _context.Instruments
+                           select m).ToList();
                              
         }
     }
