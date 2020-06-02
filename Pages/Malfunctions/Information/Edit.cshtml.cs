288f9e721e527ec454be832400627c542b98645d
mayangjie <mayangjie@kingmen.com.cn>
2020-06-02 13:49:24 +0800

    优化上传功能

--- Pages/Malfunctions/Information/Edit.cshtml.cs
+++ Pages/Malfunctions/Information/Edit.cshtml.cs
@@ -70,97 +70,32 @@ namespace EquipmentManagementSystem.Pages.Malfunctions.Information
                 return NotFound();
             }
 
-            if (FileUpload.FormFile != null && FileUpload.FormFile.Length > 0)
-            {
-                var formFileContent =
-                    await FileHelpers.ProcessFormFile<Upload>(
-                        FileUpload.FormFile, ModelState, _fileSizeLimit);
-
-                if (!ModelState.IsValid)
-                {
-                    return Page();
-                }
-
-                if (await TryUpdateModelAsync<MalfunctionInfo>(
+            if (await TryUpdateModelAsync<MalfunctionInfo>(
                     MalfunctionInfo,
                     "MalfunctionInfo",
                     i => i.BeginTime, i => i.FoundedTime, i => i.Type, i => i.Part,
                     i => i.Phenomenon, i => i.Reason, i => i.Remark, i => i.IsConfirm))
+            {
+                if (FileUpload.FormFile != null && FileUpload.FormFile.Length > 0)
                 {
+                    var formFileContent =
+                        await FileHelpers.ProcessFormFile<Upload>(
+                            FileUpload.FormFile, ModelState, _fileSizeLimit);
+
+                    if (!ModelState.IsValid)
+                    {
+                        return Page();
+                    }
+
                     MalfunctionInfo.Attachment = formFileContent;
                     MalfunctionInfo.FileName = FileUpload.FormFile.FileName;
                     MalfunctionInfo.UploadTime = DateTime.Now;
-
-                    await _context.SaveChangesAsync();
-                    return RedirectToPage("../WorkOrders/Details", new { id = MalfunctionInfo.MalfunctionWorkOrderID });
                 }
-            }
-            else
-            {
-                if (await TryUpdateModelAsync<MalfunctionInfo>(
-                    MalfunctionInfo,
-                    "MalfunctionInfo",
-                    i => i.BeginTime, i => i.FoundedTime, i => i.Type, i => i.Part,
-                    i => i.Phenomenon, i => i.Reason, i => i.Remark, i => i.IsConfirm))
-                {
-                    await _context.SaveChangesAsync();
-                    return RedirectToPage("../WorkOrders/Details", new { id = MalfunctionInfo.MalfunctionWorkOrderID });
-                }
-            }
 
-
+                await _context.SaveChangesAsync();
+                return RedirectToPage("../WorkOrders/Details", new { id = MalfunctionInfo.MalfunctionWorkOrderID });
+            }
             return Page();
         }
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
-        //public async Task<IActionResult> OnPostAsync()
-        //{
-
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return Page();
-        //    }
-
-        //    if (FileUpload.FormFile != null && FileUpload.FormFile.Length > 0)
-        //    {
-        //        var formFileContent =
-        //            await FileHelpers.ProcessFormFile<Upload>(
-        //                FileUpload.FormFile, ModelState, _fileSizeLimit);
-
-        //        if (!ModelState.IsValid)
-        //        {
-        //            return Page();
-        //        }
-
-        //        MalfunctionInfo.UploadTime = DateTime.Now;
-        //        MalfunctionInfo.Attachment = formFileContent;
-        //        MalfunctionInfo.FileName = FileUpload.FormFile.FileName;
-        //    }
-
-        //    _context.Attach(MalfunctionInfo).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!MalfunctionInfoExists(MalfunctionInfo.ID))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return RedirectToPage("../WorkOrders/Details", new { id = MalfunctionInfo.MalfunctionWorkOrderID });
-        //}
-
-        //private bool MalfunctionInfoExists(int id)
-        //{
-        //    return _context.MalfunctionInfo.Any(e => e.ID == id);
-        //}
     }
 }
