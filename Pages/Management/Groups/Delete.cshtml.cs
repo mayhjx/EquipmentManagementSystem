d493d16fe013dae36fc1016a23dadf1cb9eb4814
mayangjie <mayangjie@kingmen.com.cn>
2020-06-12 18:17:45 +0800

    新建项目组和项目管理模块

---
+++ Pages/Management/Groups/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Models;
+
+namespace EquipmentManagementSystem.Pages.Management.Groups
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly EquipmentManagementSystem.Data.EquipmentContext _context;
+
+        public DeleteModel(EquipmentManagementSystem.Data.EquipmentContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Group Group { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Group = await _context.Groups.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Group == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Group = await _context.Groups.FindAsync(id);
+
+            if (Group != null)
+            {
+                _context.Groups.Remove(Group);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Management/Projects/Delete.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace EquipmentManagementSystem.Pages.Management.Projects
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly EquipmentManagementSystem.Data.EquipmentContext _context;
+
+        public DeleteModel(EquipmentManagementSystem.Data.EquipmentContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Project Project { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Project = await _context.Projects.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Project == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Project = await _context.Projects.FindAsync(id);
+
+            if (Project != null)
+            {
+                _context.Projects.Remove(Project);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
