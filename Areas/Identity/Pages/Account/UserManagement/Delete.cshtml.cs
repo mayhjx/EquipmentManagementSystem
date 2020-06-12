703111005577117b369749b2e246e22de8409085
mayangjie <mayangjie@kingmen.com.cn>
2020-06-12 16:26:33 +0800

    增加删除功能

---
+++ Areas/Identity/Pages/Account/UserManagement/Delete.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace EquipmentManagementSystem.Areas.Identity.Pages.Account.UserManagement
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+
+        public DeleteModel(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public User userToDelete { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string name, bool? saveChangesError = false)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return NotFound();
+            }
+
+            userToDelete = await _userManager.FindByNameAsync(name);
+
+            if (userToDelete == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ErrorMessage = "删除失败，请重试。";
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return NotFound();
+            }
+
+            // 禁止删除管理员
+            if (name == "Admin")
+            {
+                return Forbid();
+            }
+
+            userToDelete = await _userManager.FindByNameAsync(name);
+
+            if (userToDelete == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                await _userManager.DeleteAsync(userToDelete);
+                return RedirectToPage("./Index");
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction("./Delete",
+                                        new { name, saveChangesError = true });
+            }
+
+        }
+    }
+}
--- Areas/Identity/Pages/Account/UserManagement/Edit.cshtml.cs
+++ Areas/Identity/Pages/Account/UserManagement/Edit.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,6 +23,14 @@ namespace EquipmentManagementSystem.Areas.Identity.Pages.Account.UserManagement
         [BindProperty]
         public User userToUpdate { get; set; }
 
+        [BindProperty]
+        public Role role { get; set; }
+
+        public class Role
+        {
+            [Display(Name = "角色")]
+            public string Name { get; set; }
+        }
         public async Task<IActionResult> OnGet(string name)
         {
             if (string.IsNullOrEmpty(name))
@@ -36,31 +45,45 @@ namespace EquipmentManagementSystem.Areas.Identity.Pages.Account.UserManagement
                 return NotFound();
             }
 
-            ViewData["Roles"] = new SelectList(_roleManager.Roles.ToList(), "Name", "Name");
+            ViewData["Roles"] = new SelectList(_roleManager.Roles.Where(u => u.Name != "Administrator").ToList(), "Name", "Name");
             return Page();
         }
 
-        //public async Task<IActionResult> OnPostAsync()
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return Page();
-        //    }
+        public async Task<IActionResult> OnPostAsync(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return NotFound();
+            }
 
-        //    var user = new User { UserName = Input.Number, Name = Input.Name, Email = Input.Email, Number = Input.Number };
-        //    var result = await _userManager.CreateAsync(user, Input.Password);
+            // 禁止编辑管理员信息
+            if (name == "Admin")
+            {
+                return Forbid();
+            }
 
-        //    if (result.Succeeded)
-        //    {
-        //        await _userManager.AddToRoleAsync(user, Input.Role);
-        //        return RedirectToPage("./Index");
-        //    }
+            userToUpdate = await _userManager.FindByNameAsync(name);
 
-        //    foreach (var error in result.Errors)
-        //    {
-        //        ModelState.AddModelError(string.Empty, error.Description);
-        //    }
-        //    return Page();
-        //}
+            if (userToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            if (await TryUpdateModelAsync<User>(
+                userToUpdate,
+                "userToUpdate",
+                i => i.Group, i => i.Name, i => i.Number, i => i.Email))
+            {
+                userToUpdate.UserName = userToUpdate.Number;
+                // 删除已关联的角色
+                await _userManager.RemoveFromRolesAsync(userToUpdate, await _userManager.GetRolesAsync(userToUpdate));
+                // 关联角色
+                await _userManager.AddToRoleAsync(userToUpdate, role.Name);
+                await _userManager.UpdateAsync(userToUpdate);
+                return RedirectToPage("./Index");
+            }
+
+            return Page();
+        }
     }
 }
