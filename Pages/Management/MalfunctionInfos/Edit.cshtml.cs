cf5262ee842405138ff50ce42e8bfaca08200a46
mayangjie <mayangjie@kingmen.com.cn>
2020-06-17 17:52:54 +0800

    增加故障部位，现象，可能原因页面

---
+++ Pages/Management/MalfunctionInfos/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Models;
+
+namespace EquipmentManagementSystem.Pages.Management.MalfunctionInfos
+{
+    public class EditModel : PageModel
+    {
+        private readonly EquipmentManagementSystem.Data.MalfunctionContext _context;
+
+        public EditModel(EquipmentManagementSystem.Data.MalfunctionContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public MalfunctionInfo MalfunctionInfo { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MalfunctionInfo = await _context.MalfunctionInfo
+                .Include(m => m.MalfunctionWorkOrder).FirstOrDefaultAsync(m => m.ID == id);
+
+            if (MalfunctionInfo == null)
+            {
+                return NotFound();
+            }
+           ViewData["MalfunctionWorkOrderID"] = new SelectList(_context.MalfunctionWorkOrder, "ID", "ID");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(MalfunctionInfo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MalfunctionInfoExists(MalfunctionInfo.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MalfunctionInfoExists(int id)
+        {
+            return _context.MalfunctionInfo.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Pages/Management/MalfunctionPhenomenon/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Models;
+
+namespace EquipmentManagementSystem.Pages.Management.MalfunctionPhenomenon
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly EquipmentManagementSystem.Data.MalfunctionContext _context;
+
+        public DetailsModel(EquipmentManagementSystem.Data.MalfunctionContext context)
+        {
+            _context = context;
+        }
+
+        public MalfunctionPhenomenon MalfunctionPhenomenon { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MalfunctionPhenomenon = await _context.MalfunctionPhenomenon.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (MalfunctionPhenomenon == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
