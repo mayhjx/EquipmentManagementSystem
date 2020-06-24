5336389b590aa3a1c32227164e1123663c538376
mayangjie <mayangjie@kingmen.com.cn>
2020-06-24 14:30:23 +0800

    Model增加故障解决措施

---
+++ Models/MalfunctionSolution.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EquipmentManagementSystem.Models
+{
+    public class MalfunctionSolution
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "解决措施")]
+        public string Solution { get; set; }
+    }
+}
