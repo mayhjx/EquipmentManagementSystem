14676bf7b8b568753fa5db097c17c13722bc238e
mayangjie <mayangjie@kingmen.com.cn>
2020-06-30 16:18:23 +0800

    增加操作手册和说明

---
+++ Pages/Manual.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace EquipmentManagementSystem.Pages
+{
+    [AllowAnonymous]
+    public class ManualModel : PageModel
+    {
+        private readonly ILogger<ManualModel> _logger;
+
+        public ManualModel(ILogger<ManualModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
