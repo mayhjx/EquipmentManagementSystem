c44d806b152e2f340191e70c25d7124d641b6adf
mayangjie <mayangjie@kingmen.com.cn>
2020-07-09 11:58:38 +0800

    增加故障工单查询 时间范围初始化为1个月 前端计算总时长

--- Pages/ReportSystem/Reporting.cshtml.cs
+++ Pages/ReportSystem/Reporting.cshtml.cs
@@ -37,6 +37,9 @@ namespace EquipmentManagementSystem.Pages.ReportSystem
             {
                 GroupSelectList = new SelectList(_context.Groups.Where(m => m.Name == userGroup), "Name", "Name");
             }
+
+            // 初始化时间范围
+            Search = new SearchForm();
         }
 
         /// <summary>
@@ -70,8 +73,8 @@ namespace EquipmentManagementSystem.Pages.ReportSystem
         [BindProperty]
         public SearchForm Search { get; set; }
 
-        [BindProperty]
         public IList<UsageRecord> UsageRecords { get; set; }
+        public IList<MalfunctionWorkOrder> MalfunctionWorkOrders { get; set; }
 
         public SelectList InstrumentSelectList { get; set; }
 
@@ -86,19 +89,41 @@ namespace EquipmentManagementSystem.Pages.ReportSystem
                 return Page();
             }
 
-            UsageRecords = (from record in _context.UsageRecords
-                            .Include(record => record.Instrument)
-                            .Include(record => record.Project)
-                            where record.Instrument.Group == Search.Group
-                            where record.InstrumentId == Search.Instrument
-                            where record.ProjectName == Search.Project
-                            where record.BeginTimeOfTest > Search.BeginTime
-                            where record.BeginTimeOfTest < Search.EndTime
-                            select record)
-                            .ToList();
+            if (Search.Category == Category.Usage)
+            {
+                UsageRecords = (from record in _context.UsageRecords
+                                .AsNoTracking()
+                                .Include(record => record.Instrument)
+                                .Include(record => record.Project)
+                                where record.Instrument.Group == Search.Group
+                                where record.InstrumentId == Search.Instrument
+                                where record.ProjectName == Search.Project
+                                where record.BeginTimeOfTest >= Search.BeginTime
+                                where record.BeginTimeOfTest < Search.EndTime.AddDays(1)
+                                select record)
+                                .ToList();
+            }
+
+            if (Search.Category == Category.Malfunction)
+            {
+                MalfunctionWorkOrders = (from record in _context.Set<MalfunctionWorkOrder>()
+                                        .AsNoTracking()
+                                        .Include(m => m.MalfunctionInfo)
+                                        .Include(m => m.Investigation)
+                                        .Include(m => m.RepairRequest)
+                                        .Include(m => m.AccessoriesOrder)
+                                        .Include(m => m.Maintenance)
+                                        .Include(m => m.Validation)
+                                        .Include(m => m.Instrument)
+                                         where record.Instrument.Group == Search.Group
+                                         where record.InstrumentID == Search.Instrument
+                                         where record.CreatedTime >= Search.BeginTime
+                                         where record.CreatedTime < Search.EndTime.AddDays(1)
+                                         select record)
+                                        .ToList();
+            }
 
             var isAdmin = User.IsInRole(Constants.ManagerRole) || User.IsInRole(Constants.DirectorRole);
-            var userGroup = _userManager.GetUserAsync(User).Result?.Group;
 
             if (isAdmin)
             {
@@ -106,7 +131,7 @@ namespace EquipmentManagementSystem.Pages.ReportSystem
             }
             else
             {
-                GroupSelectList = new SelectList(_context.Groups.Where(m => m.Name == userGroup), "Name", "Name", Search.Group);
+                GroupSelectList = new SelectList(_context.Groups.Where(m => m.Name == Search.Group), "Name", "Name", Search.Group);
             }
 
             InstrumentSelectList = new SelectList(_context.Instruments.Where(m => m.Group == Search.Group), "ID", "ID", Search.Instrument);
@@ -117,15 +142,19 @@ namespace EquipmentManagementSystem.Pages.ReportSystem
 
         public class SearchForm
         {
+            [Required(ErrorMessage = "请输入类别")]
+            [Display(Name = "类别")]
+            public Category Category { get; set; }
+
             [Required(ErrorMessage = "请输入起始时间")]
             [Display(Name = "起始时间")]
             [DataType(DataType.Date)]
-            public DateTime BeginTime { get; set; }
+            public DateTime BeginTime { get; set; } = DateTime.Now.AddDays(-30);
 
             [Required(ErrorMessage = "请输入结束时间")]
             [Display(Name = "结束时间")]
             [DataType(DataType.Date)]
-            public DateTime EndTime { get; set; }
+            public DateTime EndTime { get; set; } = DateTime.Now;
 
             [Required(ErrorMessage = "请选择一个项目组")]
             [Display(Name = "项目组")]
@@ -135,9 +164,16 @@ namespace EquipmentManagementSystem.Pages.ReportSystem
             [Display(Name = "仪器编号")]
             public string Instrument { get; set; }
 
-            [Required(ErrorMessage = "请选择一个项目名称")]
             [Display(Name = "项目名称")]
             public string Project { get; set; }
+        }
+
+        public enum Category
+        {
+            [Display(Name = "使用登记")]
+            Usage,
+            [Display(Name = "故障工单")]
+            Malfunction,
         }
     }
 }
