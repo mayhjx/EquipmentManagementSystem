372b6a1582ade548bc0e3552b841c2aaa280b73f
mayangjie <mayangjie@kingmen.com.cn>
2020-07-13 17:52:54 +0800

    Migration

--- Data/Migrations/Equipment/20200713091453_RemoveProjectFK.Designer.cs
+++ Data/Migrations/Equipment/20200713091453_RemoveProjectFK.Designer.cs
@@ -4,14 +4,16 @@ using EquipmentManagementSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EquipmentManagementSystem.Migrations.Equipment
 {
     [DbContext(typeof(EquipmentContext))]
-    partial class EquipmentContextModelSnapshot : ModelSnapshot
+    [Migration("20200713091453_RemoveProjectFK")]
+    partial class RemoveProjectFK
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
