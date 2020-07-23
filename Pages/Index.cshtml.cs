3228eb8bb7005ff8d6029d60d1495e99c8c620b1
mayangjie <mayangjie@kingmen.com.cn>
2020-07-23 17:46:50 +0800

    增加故障率和使用率统计图模板

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,38 +1,41 @@
-﻿using EquipmentManagementSystem.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EquipmentManagementSystem.Data;
 using EquipmentManagementSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace EquipmentManagementSystem.Pages
 {
     [AllowAnonymous]
     public class IndexModel : PageModel
     {
-        private readonly EquipmentContext _context;
+        private readonly EquipmentContext _equipmentContext;
         private readonly MalfunctionContext _malfunctionContext;
 
         public IndexModel(EquipmentContext context, MalfunctionContext malfunctionContext)
         {
-            _context = context;
+            _equipmentContext = context;
             _malfunctionContext = malfunctionContext;
         }
 
+        // 主检设备数量
         public int InstrumentNumber { get; set; }
 
-        // 快到期设备
+        // 待校准的主检设备
         public IList<Instrument> InstrumentOfExpire { get; set; }
 
+        // 待跟进的故障工单
         public int MalfunctionWorkOrderOfFollowNumber { get; set; }
 
+        // 故障
         public void OnGet()
         {
-            InstrumentNumber = _context.Instruments.Count();
+            InstrumentNumber = _equipmentContext.Instruments.Count();
 
-            InstrumentOfExpire = (from m in _context.Instruments
+            InstrumentOfExpire = (from m in _equipmentContext.Instruments
                                 .AsNoTracking()
                                 .Include(m => m.Calibrations)
                                 .AsEnumerable()
