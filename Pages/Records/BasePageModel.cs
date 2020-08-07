84cd4981515139ebce7471291b2f8370f85037a7
mayangjie <mayangjie@kingmen.com.cn>
2020-08-07 17:52:38 +0800

    修改记录页

---
+++ Pages/Records/BasePageModel.cs
@@ -0,0 +1,25 @@
+﻿using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EquipmentManagementSystem.Pages.Records
+{
+    public class BasePageModel : PageModel
+    {
+        protected EquipmentContext _context { get; }
+        protected UserManager<User> _userManager { get; }
+        protected IAuthorizationService _authorizationService { get; }
+
+        public BasePageModel(
+            EquipmentContext context,
+            UserManager<User> userManager,
+            IAuthorizationService authorizationService)
+        {
+            _context = context;
+            _userManager = userManager;
+            _authorizationService = authorizationService;
+        }
+    }
+}
