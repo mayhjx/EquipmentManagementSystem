ce6fbe88e1ff141196e2a586e25a32b2a01aae2d
mayangjie <mayangjie@kingmen.com.cn>
2020-09-10 17:59:02 +0800

    删除时同时删除已上传文件

--- Pages/Equipments/Acceptance/Delete.cshtml.cs
+++ Pages/Equipments/Acceptance/Delete.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using EquipmentManagementSystem.Models;
+using EquipmentManagementSystem.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -22,39 +23,11 @@ namespace EquipmentManagementSystem.Pages.Equipments.Acceptance
         public IActionResult OnGet(int? id)
         {
             return RedirectToPage("Index");
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
-
-            //InstrumentAcceptance = await _context.InstrumentAcceptances
-            //    .FirstOrDefaultAsync(m => m.Id == id);
-
-            //if (InstrumentAcceptance == null)
-            //{
-            //    return NotFound();
-            //}
-            //return Page();
         }
 
         public IActionResult OnPost(int? id)
         {
             return RedirectToPage("Index");
-
-            //if (id == null)
-            //{
-            //    return NotFound();
-            //}
-
-            //InstrumentAcceptance = await _context.InstrumentAcceptances.FindAsync(id);
-
-            //if (InstrumentAcceptance != null)
-            //{
-            //    _context.InstrumentAcceptances.Remove(InstrumentAcceptance);
-            //    await _context.SaveChangesAsync();
-            //}
-
-            //return RedirectToPage("./Index");
         }
 
         public async Task<IActionResult> OnPostDeleteAsync(int id)
@@ -76,6 +49,25 @@ namespace EquipmentManagementSystem.Pages.Equipments.Acceptance
 
             try
             {
+                // 删除已上传的文件
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.FeasibilityReportFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.ConfigurationListFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.PuchaseRequisitionFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.InstallationNoteFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.InventoryCertificateFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.FactoryAcceptanceCertificateFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.ServiceReportFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.TrainingSignInFormFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.EvaluationReportFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.EquipmentResumeFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.EquipmentFilesListFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.EquipmentCertificateFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.FactoryProductionLicenseFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.BusinessLicenseFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.MedicalDeviceRegistrationCertificateFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.EquipmentCalibrationReportFilePath);
+                FileHelpers.DeleteOlderFile(instrumentAcceptanceToDelete.EquipmentAcceptanceReportFilePath);
+
                 _context.InstrumentAcceptances.Remove(instrumentAcceptanceToDelete);
                 await _context.SaveChangesAsync();
                 return new JsonResult("删除成功！");
