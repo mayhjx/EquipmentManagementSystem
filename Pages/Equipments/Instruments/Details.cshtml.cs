682e99af2708e649a35418e00fedc6ad2ca69835
mayangjie <mayangjie@kingmen.com.cn>
2020-09-15 16:27:34 +0800

    主检设备增加档案信息

--- Pages/Equipments/Instruments/Details.cshtml.cs
+++ Pages/Equipments/Instruments/Details.cshtml.cs
@@ -1,9 +1,9 @@
-﻿using EquipmentManagementSystem.Data;
+﻿using System.Threading.Tasks;
+using EquipmentManagementSystem.Data;
 using EquipmentManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
 
 namespace EquipmentManagementSystem.Pages.Instruments
 {
@@ -18,9 +18,10 @@ namespace EquipmentManagementSystem.Pages.Instruments
 
         public Instrument Instrument { get; set; }
 
+        public InstrumentAcceptance InstrumentAcceptance { get; set; }
+
         public async Task<IActionResult> OnGetAsync(string id)
         {
-            // TODO: 如果故障信息包含的附件大于1MB，下面的语句会耗费800ms左右
             Instrument = await _context.Instruments
                                 .AsNoTracking()
                                 //.Include(a => a.Assert)
@@ -29,6 +30,10 @@ namespace EquipmentManagementSystem.Pages.Instruments
                                 .Include(d => d.MalfunctionWorkOrder)
                                     .ThenInclude(e => e.MalfunctionInfo)
                                 .FirstOrDefaultAsync(m => m.ID == id);
+
+            InstrumentAcceptance = await _context.InstrumentAcceptances
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync(m => m.InstrumentID == id);
 
             if (Instrument == null)
             {
