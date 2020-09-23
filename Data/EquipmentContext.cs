6049b4fed15c8c7ed7a63c6c11b7c4fdcba72876
mayangjie <mayangjie@kingmen.com.cn>
2020-09-23 15:34:54 +0800

    审计跟踪功能，在每次SaveChanges时记录新建修改和删除事件 参考： url1:https://www.meziantou.net/entity-framework-core-history-audit-table.htm url2:https://www.exceptionnotfound.net/entity-change-tracking-using-dbcontext-in-entity-framework-6/

--- Data/EquipmentContext.cs
+++ Data/EquipmentContext.cs
@@ -1,13 +1,28 @@
-﻿using EquipmentManagementSystem.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using EquipmentManagementSystem.Models;
+using EquipmentManagementSystem.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Newtonsoft.Json;
 
 namespace EquipmentManagementSystem.Data
 {
+
     public class EquipmentContext : DbContext
     {
-        public EquipmentContext(DbContextOptions<EquipmentContext> options)
+        private string _userId;
+        private string _userName;
+        public EquipmentContext(DbContextOptions<EquipmentContext> options, UserResolverService userService)
             : base(options)
         {
+            _userId = userService.GetUserId();
+            _userName = userService.GetUserName();
         }
 
         public DbSet<Instrument> Instruments { get; set; }
@@ -20,7 +35,7 @@ namespace EquipmentManagementSystem.Data
         public DbSet<MaintenanceRecord> MaintenanceRecords { get; set; }
         public DbSet<MaintenanceContent> MaintenanceContents { get; set; }
         public DbSet<InstrumentAcceptance> InstrumentAcceptances { get; set; }
-
+        public DbSet<AuditTrailLog> AuditTrailLogs { get; set; }
 
         //public DbSet<Computer> Computers { get; set; }
 
@@ -39,9 +54,160 @@ namespace EquipmentManagementSystem.Data
             modelBuilder.Entity<MaintenanceContent>().ToTable("MaintenanceContent");
             modelBuilder.Entity<MaintenanceRecord>().ToTable("MaintenanceRecords");
             modelBuilder.Entity<InstrumentAcceptance>().ToTable("InstrumentAcceptance");
+            modelBuilder.Entity<AuditTrailLog>().ToTable("AuditTrailLog");
 
             //modelBuilder.Entity<Computer>().ToTable("Computer");
         }
 
+        public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            var auditEntries = OnBeforeSaveChanges();
+            var result = await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+            await OnAfterSaveChanges(auditEntries);
+            return result;
+        }
+
+        public List<AuditEntry> OnBeforeSaveChanges()
+        {
+            var auditEntries = new List<AuditEntry>();
+
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.Entity is AuditTrailLog ||
+                    entry.State == EntityState.Detached ||
+                    entry.State == EntityState.Unchanged)
+                {
+                    continue;
+                }
+
+                var auditEntry = new AuditEntry(entry)
+                {
+                    UserId = _userId,
+                    UserName = _userName,
+                    EntityName = entry.Entity.GetType().Name
+                };
+                auditEntries.Add(auditEntry);
+
+                foreach (var property in entry.Properties)
+                {
+                    if (property.IsTemporary)
+                    {
+                        auditEntry.TemporaryProperties.Add(property);
+                        continue;
+                    }
+
+                    // 获取属性的displayAttribute的Name
+                    var propMemberInfo = entry.Entity.GetType().GetMember(property.Metadata.Name).FirstOrDefault();
+                    string propertyName = GetDisplayName(propMemberInfo);
+
+                    if (property.Metadata.IsPrimaryKey())
+                    {
+                        auditEntry.PrimaryKeyValue = property.CurrentValue.ToString();
+                        continue;
+                    }
+
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            auditEntry.Action = EntityState.Added.ToString();
+                            auditEntry.CurrentValue[propertyName] = property.CurrentValue;
+                            break;
+                        case EntityState.Deleted:
+                            auditEntry.Action = EntityState.Deleted.ToString();
+                            auditEntry.OriginalValue[propertyName] = property.OriginalValue;
+                            break;
+                        case EntityState.Modified:
+                            if (property.IsModified)
+                            {
+                                auditEntry.Action = EntityState.Modified.ToString();
+                                auditEntry.OriginalValue[propertyName] = property.OriginalValue;
+                                auditEntry.CurrentValue[propertyName] = property.CurrentValue;
+                            }
+                            break;
+                    }
+                }
+            }
+
+            foreach (var auditEntry in auditEntries.Where(_ => !_.HasTemporaryProperties))
+            {
+                AuditTrailLogs.Add(auditEntry.ToAudit());
+            }
+            return auditEntries.Where(_ => _.HasTemporaryProperties).ToList();
+        }
+
+        public static string GetDisplayName(MemberInfo memberInfo)
+        {
+            // 返回DisplayAttribute名或者属性名
+            var attrName = memberInfo.Name;
+            DisplayAttribute attr = Attribute.GetCustomAttribute(memberInfo, typeof(DisplayAttribute)) as DisplayAttribute;
+
+            if (attr != null)
+            {
+                attrName = attr.Name;
+            }
+            return attrName;
+        }
+
+        private Task OnAfterSaveChanges(List<AuditEntry> auditEntries)
+        {
+            if (auditEntries == null || auditEntries.Count == 0)
+            {
+                return Task.CompletedTask;
+            }
+
+            foreach (var auditEntry in auditEntries)
+            {
+                foreach (var prop in auditEntry.TemporaryProperties)
+                {
+                    if (prop.Metadata.IsPrimaryKey())
+                    {
+                        auditEntry.PrimaryKeyValue = prop.CurrentValue.ToString();
+                    }
+                    else
+                    {
+                        auditEntry.CurrentValue[prop.Metadata.Name] = prop.CurrentValue;
+                    }
+                    AuditTrailLogs.Add(auditEntry.ToAudit());
+                }
+            }
+            return SaveChangesAsync();
+        }
+    }
+
+    public class AuditEntry
+    {
+        public AuditEntry(EntityEntry entry)
+        {
+            Entry = entry;
+        }
+
+        public EntityEntry Entry { get; }
+        public string Action { get; set; }
+        public DateTime DateChanged { get; set; } = DateTime.Now;
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string EntityName { get; set; }
+        public string PrimaryKeyValue { get; set; }
+        public Dictionary<string, object> OriginalValue { get; } = new Dictionary<string, object>();
+        public Dictionary<string, object> CurrentValue { get; } = new Dictionary<string, object>();
+        public List<PropertyEntry> TemporaryProperties { get; } = new List<PropertyEntry>();
+
+        public bool HasTemporaryProperties => TemporaryProperties.Any();
+
+        public AuditTrailLog ToAudit()
+        {
+            var audit = new AuditTrailLog()
+            {
+                Action = Action,
+                DateChanged = DateChanged,
+                UserId = UserId,
+                UserName = UserName,
+                EntityName = EntityName,
+                PrimaryKeyValue = PrimaryKeyValue,
+                OriginalValue = OriginalValue.Count == 0 ? null : JsonConvert.SerializeObject(OriginalValue),
+                CurrentValue = CurrentValue.Count == 0 ? null : JsonConvert.SerializeObject(CurrentValue),
+            };
+            return audit;
+        }
     }
 }
