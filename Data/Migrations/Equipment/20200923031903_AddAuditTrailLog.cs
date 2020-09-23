38329321c14eac032e6461009692ba689988a9b8
mayangjie <mayangjie@kingmen.com.cn>
2020-09-23 15:31:20 +0800

    增加AuditTrailLog Model

---
+++ Data/Migrations/Equipment/20200923031903_AddAuditTrailLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EquipmentManagementSystem.Migrations.Equipment
+{
+    public partial class AddAuditTrailLog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditTrailLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Action = table.Column<string>(nullable: true),
+                    DateChanged = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: true),
+                    EntityName = table.Column<string>(nullable: true),
+                    PrimaryKeyValue = table.Column<string>(nullable: true),
+                    OriginalValue = table.Column<string>(nullable: true),
+                    CurrentValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditTrailLog", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditTrailLog");
+        }
+    }
+}
---
+++ Models/AuditTrailLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EquipmentManagementSystem.Models
+{
+    public class AuditTrailLog
+    {
+        public int Id { get; set; }
+        public string Action { get; set; }
+        public DateTime DateChanged { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string EntityName { get; set; }
+        public string PrimaryKeyValue { get; set; }
+        public string OriginalValue { get; set; }
+        public string CurrentValue { get; set; }
+    }
+}
