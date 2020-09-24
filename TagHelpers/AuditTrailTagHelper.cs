e443b36b833d4ab985558bca884892c7475117d2
mayangjie <mayangjie@kingmen.com.cn>
2020-09-24 09:38:33 +0800

    优化审计跟踪日志，包含按钮和Modal的代码

--- TagHelpers/AuditTrailTagHelper.cs
+++ TagHelpers/AuditTrailTagHelper.cs
@@ -8,23 +8,40 @@ namespace EquipmentManagementSystem.TagHelpers
     public class AuditTrailTagHelper : TagHelper
     {
         public IList<AuditTrailLog> logs { get; set; }
+        public string ModalId { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            output.TagName = "table";
-            output.Attributes.SetAttribute("class", "table table-bordered table-sm");
+            output.TagName = "span";
+            //output.Attributes.SetAttribute("class", );
 
-            output.Content.SetHtmlContent($@"<tbody>");
+            output.Content.SetHtmlContent($@"
+                <button type='button' class='btn btn-outline-secondary' data-toggle='modal' data-target='#{ModalId}' style='margin-right:10px;margin-left:10px; '>
+                    历史
+                </button> ");
+
+            output.Content.AppendHtml($@"
+                <div class='modal fade' id='{ModalId}' style='display:none;' aria-hidden='true'>
+                   <div class='modal-dialog modal-xl modal-dialog-scrollable'>
+                    <div class='modal-content'>
+                        <div class='modal-header'>
+                            <h4 class='modal-title'>操作日志</h4>
+                            <button type = 'button' class='close' data-dismiss='modal' aria-label='Close'>
+                                <span aria-hidden='true'>×</span>
+                            </button>
+                        </div>
+                        <div class='modal-body'>");
 
             foreach (var log in logs)
             {
-                output.Content.AppendHtml($@"
-                                            <tr class='bg-secondary text-center'>
+                output.Content.AppendHtml($@"<table class='table table-bordered table-sm table-striped'>
+                                            <tbody>
+                                            <tr class='text-center'>
                                             <td>时间：{log.DateChanged}</td>
                                             <td>动作：{log.Action}</td>
                                             <td>用户ID：{log.UserId}，用户名：{log.UserName}</td>
-                                            <td>记录ID：{log.PrimaryKeyValue}</td></tr>
-                                            <tr class='text-center bg-secondary'><td colspan='2'>旧值</td><td colspan='2'>新值</td>");
+                                            <td>记录ID：{log.PrimaryKeyValue}</td></tr>");
+                //< tr class='text-center bg-secondary'><td colspan='2'>旧值</td><td colspan='2'>新值</td>");
 
                 output.Content.AppendHtml("<tr><td colspan='2'>");
 
@@ -55,7 +72,15 @@ namespace EquipmentManagementSystem.TagHelpers
 
                 output.Content.AppendHtml("</td></tr>");
             }
-            output.Content.AppendHtml("</tbody>");
+            output.Content.AppendHtml("</tbody></table>");
+
+            output.Content.AppendHtml($@"</div>
+                        <div class='modal-footer'>
+                            <button type='button' class='btn btn-primary float-right' data-dismiss='modal'>关闭</button>
+                        </div>
+                    </div>
+                </div>
+            </div>");
             output.TagMode = TagMode.StartTagAndEndTag;
         }
     }
