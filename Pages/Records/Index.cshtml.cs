4a1d3ea103b8cfe08e069c011b738840c138fece
mayangjie <mayangjie@kingmen.com.cn>
2020-10-15 17:47:11 +0800

    优化维护使用等于模块显示“所有记录”的功能

--- Pages/Records/Index.cshtml.cs
+++ Pages/Records/Index.cshtml.cs
@@ -115,86 +115,86 @@ namespace EquipmentManagementSystem.Pages.Records
             return Result;
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        public IActionResult OnPost()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            var usageRecord = from record in _context.UsageRecords
-                            .AsNoTracking()
-                            .Include(record => record.Instrument)
-                            .Include(record => record.Project)
-                                .ThenInclude(p => p.Group)
-                              select record;
-
-            var maintenanceRecord = from record in _context.MaintenanceRecords
-                                    .AsNoTracking()
-                                    .Include(record => record.Instrument)
-                                    .Include(record => record.Project)
-                                        .ThenInclude(p => p.Group)
-                                    select record;
-
-            // 显示使用中的记录
-            if (Search.Status == Status.Using)
-            {
-                usageRecord = usageRecord.Where(record => record.EndTime == null);
-                maintenanceRecord = maintenanceRecord.Where(record => record.EndTime == null);
-            }
-
-            //if (Search.Instrument != null)
+            //if (!ModelState.IsValid)
             //{
-            //    usageRecord = usageRecord.Where(u => u.InstrumentId == Search.Instrument);
-            //    maintenanceRecord = maintenanceRecord.Where(u => u.InstrumentId == Search.Instrument);
+            //    return Page();
             //}
 
-            //if (Search.Project != null)
+            //var usageRecord = from record in _context.UsageRecords
+            //                .AsNoTracking()
+            //                .Include(record => record.Instrument)
+            //                .Include(record => record.Project)
+            //                    .ThenInclude(p => p.Group)
+            //                  select record;
+
+            //var maintenanceRecord = from record in _context.MaintenanceRecords
+            //                        .AsNoTracking()
+            //                        .Include(record => record.Instrument)
+            //                        .Include(record => record.Project)
+            //                            .ThenInclude(p => p.Group)
+            //                        select record;
+
+            //// 显示使用中的记录
+            //if (Search.Status == Status.Using)
             //{
-            //    usageRecord = usageRecord.Where(u => u.ProjectName == Search.Project);
-            //    maintenanceRecord = maintenanceRecord.Where(u => u.ProjectName == Search.Project);
+            //    usageRecord = usageRecord.Where(record => record.EndTime == null);
+            //    maintenanceRecord = maintenanceRecord.Where(record => record.EndTime == null);
             //}
 
-            var projects = _context.Projects.AsNoTracking().Include(p => p.Group);
+            ////if (Search.Instrument != null)
+            ////{
+            ////    usageRecord = usageRecord.Where(u => u.InstrumentId == Search.Instrument);
+            ////    maintenanceRecord = maintenanceRecord.Where(u => u.InstrumentId == Search.Instrument);
+            ////}
 
-            var isAuthorized = User.IsInRole(Constants.DirectorRole) || User.IsInRole(Constants.ManagerRole);
+            ////if (Search.Project != null)
+            ////{
+            ////    usageRecord = usageRecord.Where(u => u.ProjectName == Search.Project);
+            ////    maintenanceRecord = maintenanceRecord.Where(u => u.ProjectName == Search.Project);
+            ////}
 
-            if (User.Identity.IsAuthenticated && !isAuthorized)
-            {
-                var currentUserGroup = (await _userManager.GetUserAsync(User)).Group;
+            //var projects = _context.Projects.AsNoTracking().Include(p => p.Group);
 
-                var projectsOfcurrentUserGroup = await projects.AsNoTracking().Where(p => p.Group.Name == currentUserGroup).Select(p => p.Name).ToListAsync();
+            //var isAuthorized = User.IsInRole(Constants.DirectorRole) || User.IsInRole(Constants.ManagerRole);
 
-                //ProjectSelectList = new SelectList(projects.AsNoTracking().Where(p => p.Group.Name == currentUserGroup).OrderBy(p => p.Name), "Name", "Name");
-
-                // 显示当前用户所属项目组的记录
-                usageRecord = from record in usageRecord
-                              where projectsOfcurrentUserGroup.Contains(record.ProjectName)
-                              select record;
-
-                maintenanceRecord = from record in maintenanceRecord
-                                    where projectsOfcurrentUserGroup.Contains(record.ProjectName)
-                                    select record;
-            }
-            //else
+            //if (User.Identity.IsAuthenticated && !isAuthorized)
             //{
-            //    //ProjectSelectList = new SelectList(projects.AsNoTracking().OrderBy(p => p.Name), "Name", "Name");
+            //    var currentUserGroup = (await _userManager.GetUserAsync(User)).Group;
+
+            //    var projectsOfcurrentUserGroup = await projects.AsNoTracking().Where(p => p.Group.Name == currentUserGroup).Select(p => p.Name).ToListAsync();
+
+            //    //ProjectSelectList = new SelectList(projects.AsNoTracking().Where(p => p.Group.Name == currentUserGroup).OrderBy(p => p.Name), "Name", "Name");
+
+            //    // 显示当前用户所属项目组的记录
+            //    usageRecord = from record in usageRecord
+            //                  where projectsOfcurrentUserGroup.Contains(record.ProjectName)
+            //                  select record;
+
+            //    maintenanceRecord = from record in maintenanceRecord
+            //                        where projectsOfcurrentUserGroup.Contains(record.ProjectName)
+            //                        select record;
             //}
+            ////else
+            ////{
+            ////    //ProjectSelectList = new SelectList(projects.AsNoTracking().OrderBy(p => p.Name), "Name", "Name");
+            ////}
 
-            UsageRecords = await usageRecord.OrderByDescending(m => m.BeginTimeOfTest).ToListAsync();
-            MaintenanceRecords = await maintenanceRecord.OrderByDescending(m => m.BeginTime).ToListAsync();
+            //UsageRecords = await usageRecord.OrderByDescending(m => m.BeginTimeOfTest).ToListAsync();
+            //MaintenanceRecords = await maintenanceRecord.OrderByDescending(m => m.BeginTime).ToListAsync();
 
-            MaintenanceAuditTrailLogs = await _context.AuditTrailLogs
-                .AsNoTracking()
-                .Where(l => l.EntityName == MaintenanceRecord.GetType().Name)
-                .OrderByDescending(l => l.DateChanged)
-                .ToListAsync();
+            //MaintenanceAuditTrailLogs = await _context.AuditTrailLogs
+            //    .AsNoTracking()
+            //    .Where(l => l.EntityName == MaintenanceRecord.GetType().Name)
+            //    .OrderByDescending(l => l.DateChanged)
+            //    .ToListAsync();
 
-            UsageAuditTrailLogs = await _context.AuditTrailLogs
-                .AsNoTracking()
-                .Where(l => l.EntityName == UsageRecord.GetType().Name)
-                .OrderByDescending(l => l.DateChanged)
-                .ToListAsync();
+            //UsageAuditTrailLogs = await _context.AuditTrailLogs
+            //    .AsNoTracking()
+            //    .Where(l => l.EntityName == UsageRecord.GetType().Name)
+            //    .OrderByDescending(l => l.DateChanged)
+            //    .ToListAsync();
 
             return RedirectToPage("./Index", new { status = Search.Status });
         }
