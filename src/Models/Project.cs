2f4aa738da9e54740789a4d9f5dd6f6ad27a1019
mayangjie <mayangjie@kingmen.com.cn>
2020-11-04 17:51:32 +0800

    修改ProjectModel

--- src/Models/Project.cs
+++ src/Models/Project.cs
@@ -7,6 +7,7 @@ namespace EquipmentManagementSystem.Models
     {
         public int Id { get; set; }
 
+        [Required]
         [StringLength(50)]
         [Display(Name = "项目名称")]
         public string Name { get; set; }
@@ -21,19 +22,40 @@ namespace EquipmentManagementSystem.Models
         public DateTime? SimpleTestTime { get; set; }
 
         [Display(Name = "色谱柱类型")]
-        [StringLength(500)]
+        [StringLength(1000)]
         public string ColumnType { get; set; }
 
-        [Display(Name = "流动相/载气")]
-        [StringLength(500)]
-        public string Carrier { get; set; }
+        [Display(Name = "色谱柱压力个数")]
+        public int NumberOfColumn { get; set; }
 
-        [Display(Name = "离子源类型")]
-        [StringLength(50)]
+        [Display(Name = "色谱柱压力单位")]
+        [StringLength(10)]
+        public string UnitOfColumn { get; set; }
+
+        [Display(Name = "真空度个数")]
+        public int NumberOfVacuumDegree { get; set; }
+
+        [Display(Name = "真空度压力单位")]
+        [StringLength(10)]
+        public string UnitOfVacuumDegree { get; set; }
+
+        [Display(Name = "Test个数")]
+        public int NumberOfTest { get; set; }
+
+        [Display(Name = "Test压力单位")]
+        [StringLength(10)]
+        public string UnitOfTest { get; set; }
+
+        [Display(Name = "流动相/载气")]
+        [StringLength(1000)]
+        public string MobilePhase { get; set; }
+
+        [Display(Name = "离子源")]
+        [StringLength(100)]
         public string IonSource { get; set; }
 
         [Display(Name = "检测器")]
-        [StringLength(20)]
+        [StringLength(100)]
         public string Detector { get; set; }
     }
 }
