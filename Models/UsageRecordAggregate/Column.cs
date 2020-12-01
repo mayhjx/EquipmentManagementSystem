ea41f42c24c78d10a1efe1295fbf31a56c556ba4
mayangjie <mayangjie@kingmen.com.cn>
2020-12-01 11:09:37 +0800

    整理Models

---
+++ Models/UsageRecordAggregate/Column.cs
@@ -0,0 +1,9 @@
+﻿namespace EquipmentManagementSystem.Models
+{
+    public class Column
+    {
+        public string SystemNumber { get; set; }
+        public float Value { get; set; }
+        public string Unit { get; set; }
+    }
+}
