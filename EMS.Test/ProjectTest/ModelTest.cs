a371530db787cf241b308eb99d1bdfca068825dc
mayangjie <mayangjie@kingmen.com.cn>
2020-12-04 17:19:16 +0800

    Add project Test

---
+++ EMS.Test/ProjectTest/ModelTest.cs
@@ -0,0 +1,32 @@
+﻿using EquipmentManagementSystem.Models;
+using System.Collections.Generic;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace EMS.Test.ProjectTest
+{
+    public class ModelTest
+    {
+        [Fact]
+        public void SetMobilePhase_AndGet_ShouldBeSame()
+        {
+            List<string> mobilePhases = new List<string> { "A", "B" };
+            var project = new Project();
+
+            project.SetMobilePhase(mobilePhases);
+
+            Assert.Equal(mobilePhases, project.GetMobilePhase());
+        }
+
+        [Fact]
+        public void SetColumnType_AndGet_ShouldBeSame()
+        {
+            List<string> columnTypes = new List<string> { "A", "B", "C" };
+            var project = new Project();
+
+            project.SetColumnType(columnTypes);
+
+            Assert.Equal(columnTypes, project.GetColumnType());
+        }
+    }
+}
---
+++ EMS.Test/ProjectTest/RepositoryTest.cs
@@ -0,0 +1,69 @@
+﻿using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Models;
+using EquipmentManagementSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace EMS.Test.ProjectTest
+{
+    public class RepositoryTest
+    {
+        [Fact]
+        public async Task SetAndGet_ColumnTypesByName_ShouldBe_Same()
+        {
+            var options = new DbContextOptionsBuilder<EquipmentContext>()
+                .UseInMemoryDatabase(databaseName: "SetAndGet_ColumnTypesByName_ShouldBe_Same")
+                .Options;
+
+            var columnTypes = new List<string> { "A", "B" };
+
+            // Insert seed data into the database using one instance of the context
+            using (var context = Utilities.CreateContext(options))
+            {
+                var project = new Project() { Name = "Test" };
+                project.SetColumnType(columnTypes);
+                context.Projects.Add(project);
+                context.SaveChanges();
+            }
+
+            // Use a clean instance of the context to run the test
+            using (var context = Utilities.CreateContext(options))
+            {
+                var repo = new ProjectRepository(context);
+                var result = await repo.GetColumnTypesByName("Test");
+                Assert.Equal(columnTypes, result);
+            }
+        }
+
+
+        [Fact]
+        public async Task SetAndGet_MobilePhaseByName_ShouldBe_Same()
+        {
+            var options = new DbContextOptionsBuilder<EquipmentContext>()
+                .UseInMemoryDatabase(databaseName: "SetAndGet_MobilePhaseByName_ShouldBe_Same")
+                .Options;
+
+            var mobilePhases = new List<string> { "A", "B" };
+
+            // Insert seed data into the database using one instance of the context
+            using (var context = Utilities.CreateContext(options))
+            {
+                var project = new Project() { Name = "Test" };
+                project.SetMobilePhase(mobilePhases);
+                context.Projects.Add(project);
+                context.SaveChanges();
+            }
+
+            // Use a clean instance of the context to run the test
+            using (var context = Utilities.CreateContext(options))
+            {
+                var repo = new ProjectRepository(context);
+                var result = await repo.GetMobilePhasesByName("Test");
+                Assert.Equal(mobilePhases, result);
+            }
+        }
+    }
+}
