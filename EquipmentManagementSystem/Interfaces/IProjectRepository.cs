a62e08f13534ddbfe0ab6e980e2d47fce184e6cd
mayangjie <mayangjie@kingmen.com.cn>
2020-12-04 17:19:38 +0800

    Add Project Repository

---
+++ EquipmentManagementSystem/Interfaces/IProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using EquipmentManagementSystem.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EquipmentManagementSystem.Interfaces
+{
+    interface IProjectRepository : IGenericRepository<Project>
+    {
+        public Task<List<string>> GetMobilePhasesByName(string projectName);
+        public Task<List<string>> GetColumnTypesByName(string projectName);
+    }
+}
---
+++ EquipmentManagementSystem/Repositories/ProjectRepository.cs
@@ -0,0 +1,26 @@
+﻿using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Interfaces;
+using EquipmentManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EquipmentManagementSystem.Repositories
+{
+    public class ProjectRepository : GenericRepository<Project>, IProjectRepository
+    {
+        public ProjectRepository(EquipmentContext context) : base(context)
+        {
+        }
+
+        public async Task<List<string>> GetColumnTypesByName(string projectName)
+        {
+            return (await _context.Projects.FirstOrDefaultAsync(p => p.Name == projectName)).GetColumnType();
+        }
+
+        public async Task<List<string>> GetMobilePhasesByName(string projectName)
+        {
+            return (await _context.Projects.FirstOrDefaultAsync(p => p.Name == projectName)).GetMobilePhase();
+        }
+    }
+}
