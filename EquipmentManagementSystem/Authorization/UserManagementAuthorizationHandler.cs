e63c7777d4d0a1766ffb78635b60d94821c9742b
mayangjie <mayhjx@foxmail.com>
2020-12-05 18:44:56 +0800

    用户管理权限分级

---
+++ EquipmentManagementSystem/Authorization/UserManagementAuthorizationHandler.cs
@@ -0,0 +1,64 @@
+﻿using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace EquipmentManagementSystem.Authorization
+{
+    public class UserManagementAuthorizationHandler:
+        AuthorizationHandler<OperationAuthorizationRequirement, User>
+    {
+        public UserManagementAuthorizationHandler()
+        {
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                        OperationAuthorizationRequirement requirement,
+                                                        User resource)
+        {
+            if (context.User == null || resource == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (requirement.Name == Constants.CreateOperationName)
+            {
+                if (context.User.IsInRole(Constants.AdministratorRole) ||
+                    context.User.IsInRole(Constants.DirectorRole))
+                {
+                    context.Succeed(requirement);
+                }
+            }
+
+            if (requirement.Name == Constants.ReadOperationName)
+            {
+                if (context.User.IsInRole(Constants.AdministratorRole) ||
+                    context.User.IsInRole(Constants.DirectorRole))
+                {
+                    context.Succeed(requirement);
+                }
+            }
+
+            if (requirement.Name == Constants.UpdateOperationName)
+            {
+                if (context.User.IsInRole(Constants.AdministratorRole) ||
+                    context.User.IsInRole(Constants.DirectorRole))
+                {
+                    context.Succeed(requirement);
+                }
+            }
+
+            if (requirement.Name == Constants.DeleteOperationName)
+            {
+                if (context.User.IsInRole(Constants.AdministratorRole) ||
+                    context.User.IsInRole(Constants.DirectorRole))
+                {
+                    context.Succeed(requirement);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
