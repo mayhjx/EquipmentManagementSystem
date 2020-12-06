b1e11a85561a79c081593c52de921e37b9f6f0b8
mayangjie <mayhjx@foxmail.com>
2020-12-06 15:20:27 +0800

    add taghelper used to show a list<string> into ul element

---
+++ EquipmentManagementSystem/TagHelpers/ListTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Newtonsoft.Json;
+
+namespace EquipmentManagementSystem.TagHelpers
+{
+    public class ListTagHelper:TagHelper
+    {
+        public List<string> List { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ul";
+
+            foreach(var element in List)
+            {
+                output.Content.AppendHtml($"<li>{element}</li>");
+            }
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
