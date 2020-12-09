c266c74e791e43b7b299547a3106893672e8bdbe
mayangjie <mayangjie@kingmen.com.cn>
2020-12-09 17:24:26 +0800

    add method：修改Instrument的projects字段中的括号及其内容

--- EquipmentManagementSystem/Program.cs
+++ EquipmentManagementSystem/Program.cs
@@ -13,11 +13,59 @@ namespace EquipmentManagementSystem
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
+            //UpdateProjectsOfInstrument(host);
             //CreateDbIfNotExists(host);
             CreateAdminIfNotExists(host);
             CreateUserIfNotExists(host);
             host.Run();
         }
+
+        // 修改Instrument的projects字段中的括号及其内容
+        //private static void UpdateProjectsOfInstrument(IHost host)
+        //{
+        //    using var scope = host.Services.CreateScope();
+        //    var services = scope.ServiceProvider;
+
+        //    try
+        //    {
+        //        var context = services.GetRequiredService<EquipmentContext>();
+        //        var wwwrootPath = services.GetRequiredService<IWebHostEnvironment>().WebRootPath;
+
+        //        foreach (var instr in context.Instruments)
+        //        {
+        //            if (string.IsNullOrEmpty(instr.Projects)) continue;
+        //            var projectList = instr.Projects.Split(", ");
+        //            var projectRemoveShortName = new List<string>();
+        //            foreach (var project in projectList)
+        //            {
+        //                var leftIndex = project.IndexOf("(");
+        //                var rightIndex = project.IndexOf(")");
+        //                if (leftIndex > 0)
+        //                {
+        //                    var left = project.Substring(0, leftIndex);
+        //                    string right = "";
+        //                    if (rightIndex < project.Length - 1)
+        //                    {
+        //                        right = project.Substring(rightIndex + 1, project.Length - 1 - rightIndex);
+        //                    }
+        //                    projectRemoveShortName.Add($"{left}{right}");
+        //                }
+        //            }
+        //            if (projectRemoveShortName.Count > 0)
+        //            {
+        //                instr.Projects = string.Join(", ", projectRemoveShortName);
+        //            }
+        //        }
+        //        context.SaveChanges();
+        //        //DbInitializer.Initialize(context, wwwrootPath);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        var logger = services.GetRequiredService<ILogger<Program>>();
+        //        logger.LogError(ex, "An error occurred creating the DB.");
+        //        //throw ex;
+        //    }
+        //}
 
         private static void CreateDbIfNotExists(IHost host)
         {
