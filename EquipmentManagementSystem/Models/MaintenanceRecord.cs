1c54958e4bd378764ddfe05e3ab97390b79bd7ad
mayangjie <mayangjie@kingmen.com.cn>
2020-12-11 18:01:46 +0800

    Delete FK to project; Add GroupName; Remove string length limit

--- EquipmentManagementSystem/Models/MaintenanceRecord.cs
+++ EquipmentManagementSystem/Models/MaintenanceRecord.cs
@@ -11,13 +11,11 @@ namespace EquipmentManagementSystem.Models
         public string InstrumentId { get; set; }
         public Instrument Instrument { get; set; }
 
-        [Display(Name = "项目名称")]
-        [StringLength(50)]
-        public string ProjectName { get; set; }
+        [Display(Name = "项目组")]
+        public string GroupName { get; set; }
 
-        [Display(Name = "项目ID")]
-        public int? ProjectId { get; set; }
-        public Project Project { get; set; }
+        [Display(Name = "项目名称")]
+        public string ProjectName { get; set; }
 
         [Display(Name = "开始时间")]
         [DataType(DataType.DateTime)]
@@ -30,15 +28,12 @@ namespace EquipmentManagementSystem.Models
         public DateTime? EndTime { get; set; }
 
         [Display(Name = "维护类型")]
-        [StringLength(20)]
         public string Type { get; set; }
 
         [Display(Name = "维护内容")]
-        [StringLength(500)]
         public string Content { get; set; }
 
         [Display(Name = "操作者")]
-        [StringLength(10)]
         public string Operator { get; set; }
     }
 }
