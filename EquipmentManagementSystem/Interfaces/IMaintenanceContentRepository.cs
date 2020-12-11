24e993557e0d018f01ae2f99924a849033740204
mayangjie <mayangjie@kingmen.com.cn>
2020-12-11 17:57:52 +0800

    Add MaintenanceContentRepository

---
+++ EquipmentManagementSystem/Interfaces/IMaintenanceContentRepository.cs
@@ -0,0 +1,9 @@
+﻿using EquipmentManagementSystem.Models;
+using System.Collections.Generic;
+namespace EquipmentManagementSystem.Interfaces
+{
+    public interface IMaintenanceContentRepository : IGenericRepository<MaintenanceContent>
+    {
+        public List<MaintenanceContent> GetByInstrumentPlatform(string platform);
+    }
+}
---
+++ EquipmentManagementSystem/Repositories/MaintenanceContentRepository.cs
@@ -0,0 +1,20 @@
+﻿using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Interfaces;
+using EquipmentManagementSystem.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EquipmentManagementSystem.Repositories
+{
+    public class MaintenanceContentRepository : GenericRepository<MaintenanceContent>, IMaintenanceContentRepository
+    {
+        public MaintenanceContentRepository(EquipmentContext context) : base(context)
+        {
+        }
+
+        public List<MaintenanceContent> GetByInstrumentPlatform(string platform)
+        {
+            return _context.MaintenanceContents.Where(i => i.InstrumentPlatform == platform).ToList();
+        }
+    }
+}
