ab4a98fc5c88bceea9c8b78d30977437b08cfb4d
mayangjie <mayangjie@kingmen.com.cn>
2020-12-14 17:59:09 +0800

    删除使用记录和维护记录的button的class不同。

--- EquipmentManagementSystem/wwwroot/js/Records.js
+++ EquipmentManagementSystem/wwwroot/js/Records.js
@@ -1,4 +1,30 @@
 ﻿// 使用记录
+$(document).ready(function () {
+    // 重新激活上次选择的Tab
+    if (localStorage) {
+        $('div.card-title a[data-toggle="pill"').on('click', function () {
+            localStorage.setItem('activeTab', $(this).attr('href'));
+        });
+        var activeTab = localStorage.getItem('activeTab');
+        if (activeTab) {
+            $('.nav-usageRecord a[href="' + activeTab + '"]').tab('show');
+        }
+    }
+    else {
+        console.log("当前浏览器不支持LocalStorage");
+    };
+
+    $('#date').on('change', function () {
+        $('form#search').submit();
+    });
+
+    $('#instrumentId').on('change', function () {
+        $('form#search').submit();
+    });
+
+    getLatestRecord();
+})
+
 $(".edit-btn").click(function () {
     // 将其他已在编辑状态的行取消编辑
     $(".cancel-btn").click();
@@ -18,7 +44,7 @@ $(".cancel-btn").click(function () {
     RowOfViewRecord.show();
 });
 
-$(".delete-btn").click(function () {
+$(".delete-usage-record-btn").click(function () {
     if (!confirm("确认删除该条记录？")) {
         return false;
     }
@@ -85,28 +111,34 @@ function setInitialValue(data) {
     }
 }
 
-$(document).ready(function () {
-    // 重新激活上次选择的Tab
-    if (localStorage) {
-        $('div.card-title a[data-toggle="pill"').on('click', function () {
-            localStorage.setItem('activeTab', $(this).attr('href'));
-        });
-        var activeTab = localStorage.getItem('activeTab');
-        if (activeTab) {
-            $('.nav-usageRecord a[href="' + activeTab + '"]').tab('show');
-        }
+// 删除维护记录
+$(".delete-maintenance-record-btn").click(function () {
+    if (!confirm("确认删除该条记录？")) {
+        return false;
     }
-    else {
-        console.log("当前浏览器不支持LocalStorage");
+
+    let id = $(this).attr("data-id");
+    let record = $(this).closest("tr"); // 调用ajax后，this就不再是delete-btn了，所以需要先获取
+
+    var options = {};
+    options.url = "Records/MaintenanceRecords/Delete?id=" + id;
+    options.type = "post";
+    options.dataType = "json";
+    options.beforeSend = function (xhr) {
+        xhr.setRequestHeader("MY-XSRF-TOKEN",
+            $('input:hidden[name="__RequestVerificationToken"]').val());
+    };
+    options.success = function (msg) {
+        if (msg.indexOf("成功") != -1) {
+            record.remove(); // 删除行
+        }
+        else {
+            alert(msg);
+        }
+    };
+    options.error = function (msg) {
+        alert(msg);
     };
 
-    $('#date').on('change', function () {
-        $('form#search').submit();
-    });
-
-    $('#instrumentId').on('change', function () {
-        $('form#search').submit();
-    });
-
-    getLatestRecord();
-})
+    $.ajax(options);
+});
