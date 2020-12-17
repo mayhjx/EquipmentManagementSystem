145cd678fa1cc5e3d3216f663a399a0b4fe93a61
mayangjie <mayangjie@kingmen.com.cn>
2020-12-17 11:10:05 +0800

    获取用户相关的仪器编号，优化维护使用的查询

---
+++ EquipmentManagementSystem/Interfaces/IInstrumentService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace EquipmentManagementSystem.Interfaces
+{
+    public interface IInstrumentService
+    {
+        /// <summary>
+        /// 返回项目组相关的检测仪器
+        /// </summary>
+        /// <param name="group"></param>
+        /// <returns></returns>
+        List<string> GetInstrumentIdRelateToProjectsOfGroup(string group);
+    }
+}
---
+++ EquipmentManagementSystem/Services/InstrumentService.cs
@@ -0,0 +1,33 @@
+﻿using EquipmentManagementSystem.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EquipmentManagementSystem.Services
+{
+    public class InstrumentService : IInstrumentService
+    {
+        private readonly IProjectRepository _projectRepository;
+        private readonly IInstrumentRepository _instrumentRepository;
+
+        public InstrumentService(IProjectRepository projectRepository, IInstrumentRepository instrumentRepository)
+        {
+            _projectRepository = projectRepository;
+            _instrumentRepository = instrumentRepository;
+        }
+
+
+        public List<string> GetInstrumentIdRelateToProjectsOfGroup(string group)
+        {
+            var result = new List<string>();
+
+            var projects = _projectRepository.GetByGroup(group);
+
+            foreach (var project in projects)
+            {
+                var instruments = _instrumentRepository.GetAllInstrumentIdByProject(project);
+                result.AddRange(instruments);
+            }
+            return result.Distinct().ToList();
+        }
+    }
+}
