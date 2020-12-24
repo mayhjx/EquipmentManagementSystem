89ac2985f73a7d60cc80f63bc077282e95ab405a
mayangjie <mayangjie@kingmen.com.cn>
2020-12-24 17:45:27 +0800

    Add Instrument Authorization Hanlder Test

---
+++ EMS.Test/InstrumentTest/TechnicianAuthorizationTest.cs
@@ -0,0 +1,71 @@
+﻿using EquipmentManagementSystem.Authorization;
+using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace EMS.Test.InstrumentTest
+{
+    public class TechnicianAuthorizationTest
+    {
+        [Fact]
+        public async Task Handler_Instrument_Read_ShouldSucceed()
+        {
+            var resource = new Instrument { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.TechnicianRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.ReadOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new InstrumentAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_Instrument_Create_ShouldFail()
+        {
+            var resource = new Instrument { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.TechnicianRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.CreateOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new InstrumentAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.False(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_Instrument_Update_ShouldFail()
+        {
+            var resource = new Instrument { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.TechnicianRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.UpdateOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new InstrumentAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.False(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_Instrument_Delete_ShouldFail()
+        {
+            var resource = new Instrument { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.TechnicianRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.DeleteOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new InstrumentAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.False(authzContext.HasSucceeded);
+        }
+    }
+}
