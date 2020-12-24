30e4bfb3575b4867eac89cfd228bbb5026a47e38
mayangjie <mayangjie@kingmen.com.cn>
2020-12-24 17:45:39 +0800

    Add Maintenance Record Authorization Hanlder Test

---
+++ EMS.Test/MaintenanceRecordTest/DirectorAuthorizationTest.cs
@@ -0,0 +1,71 @@
+﻿using EquipmentManagementSystem.Authorization;
+using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace EMS.Test.MaintenanceRecordTest
+{
+    public class DirectorAuthorizationTest
+    {
+        [Fact]
+        public async Task Handler_MaintenanceRecord_Create_ShouldSucceed()
+        {
+            var resource = new MaintenanceRecord { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.DirectorRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.CreateOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_MaintenanceRecord_Update_ShouldSucceed()
+        {
+            var resource = new MaintenanceRecord { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.DirectorRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.UpdateOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_MaintenanceRecord_Read_ShouldSucceed()
+        {
+            var resource = new MaintenanceRecord { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.DirectorRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.ReadOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_MaintenanceRecord_Delete_ShouldSucceed()
+        {
+            var resource = new MaintenanceRecord { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.DirectorRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.DeleteOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+    }
+}
---
+++ EMS.Test/MaintenanceRecordTest/TechnicianAuthorizationTest.cs
@@ -0,0 +1,111 @@
+﻿using EquipmentManagementSystem.Authorization;
+using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace EMS.Test.MaintenanceRecordTest
+{
+    public class TechnicianAuthorizationTest
+    {
+        [Fact]
+        public async Task Handler_MaintenanceRecord_Read_ShouldSucceed()
+        {
+            var resource = new MaintenanceRecord { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.TechnicianRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.ReadOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_MaintenanceRecord_Create_ShouldSucceed()
+        {
+            var resource = new MaintenanceRecord { };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new Claim(ClaimTypes.Role, Constants.TechnicianRole) }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.CreateOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_UpdateMaintenanceRecordWithOwned_ShouldSucceed()
+        {
+            var resource = new MaintenanceRecord { Operator = "Test" };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> {
+                new Claim(ClaimTypes.Role, Constants.TechnicianRole),
+                new Claim(ClaimTypes.GivenName, "Test"),
+            }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.UpdateOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_UpdateMaintenanceRecordWithIllegalUser_ShouldFail()
+        {
+            var resource = new MaintenanceRecord { Operator = "Test 1" };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> {
+                new Claim(ClaimTypes.Role, Constants.TechnicianRole),
+                new Claim(ClaimTypes.GivenName, "Test 2"),
+            }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.UpdateOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.False(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_DeleteMaintenanceRecordWithOwner_ShouldSucceed()
+        {
+            var resource = new MaintenanceRecord { Operator = "Test" };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> {
+                new Claim(ClaimTypes.Role, Constants.TechnicianRole),
+                new Claim(ClaimTypes.GivenName, "Test"),
+            }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.DeleteOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.True(authzContext.HasSucceeded);
+        }
+
+        [Fact]
+        public async Task Handler_DeleteMaintenanceRecordWithIllegalUser_ShouldFail()
+        {
+            var resource = new MaintenanceRecord { Operator = "Test 1" };
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> {
+                new Claim(ClaimTypes.Role, Constants.TechnicianRole),
+                new Claim(ClaimTypes.GivenName, "Test 2"),
+            }));
+            var requirement = new OperationAuthorizationRequirement { Name = Constants.DeleteOperationName };
+
+            var authzContext = new AuthorizationHandlerContext(new List<IAuthorizationRequirement> { requirement }, user, resource);
+            var authzHandler = new MaintenanceRecordAuthorizationHandler();
+            await authzHandler.HandleAsync(authzContext);
+
+            Assert.False(authzContext.HasSucceeded);
+        }
+    }
+}
