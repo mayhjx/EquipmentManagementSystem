bc9cd6392fa3147fa56ebd7e133bf9b734034512
mayangjie <mayangjie@kingmen.com.cn>
2020-12-30 10:52:20 +0800

    增加故障工单接口

---
+++ EquipmentManagementSystem/Interfaces/IMalfunctionRepository.cs
@@ -0,0 +1,13 @@
+﻿using EquipmentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+
+namespace EquipmentManagementSystem.Interfaces
+{
+    public interface IMalfunctionRepository : IGenericRepository<MalfunctionWorkOrder>
+    {
+        double GetTotalMalfunctionTimeOfRecords(List<MalfunctionWorkOrder> workOrders);
+
+        List<MalfunctionWorkOrder> GetAllByInstrumentIdAndMonthOfBeginTime(string instrumentId, DateTime date);
+    }
+}
---
+++ EquipmentManagementSystem/Repositories/MalfunctionRepository.cs
@@ -0,0 +1,53 @@
+﻿using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Interfaces;
+using EquipmentManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EquipmentManagementSystem.Repositories
+{
+    public class MalfunctionRepository : GenericRepository<MalfunctionWorkOrder>, IMalfunctionRepository
+    {
+        public MalfunctionRepository(EquipmentContext context) : base(context)
+        {
+        }
+
+        public List<MalfunctionWorkOrder> GetAllByInstrumentIdAndMonthOfBeginTime(string instrumentId, DateTime date)
+        {
+            return _context.Set<MalfunctionWorkOrder>()
+                            .AsNoTracking()
+                            .Include(i => i.MalfunctionInfo)
+                            .AsEnumerable()
+                            .Where(i => i.InstrumentID == instrumentId)
+                            .Where(i => i.MalfunctionInfo.BeginTime.Year == date.Year)
+                            .Where(i => i.MalfunctionInfo.BeginTime.Month == date.Month)
+                            .ToList();
+        }
+
+        public double GetTotalMalfunctionTimeOfRecords(List<MalfunctionWorkOrder> workOrders)
+        {
+            double totalTime = 0;
+
+            foreach (var order in workOrders)
+            {
+                var workOrderWithInclude = _context.MalfunctionWorkOrder
+                .AsNoTracking()
+                .Include(w => w.MalfunctionInfo)
+                .Include(w => w.Repair)
+                .SingleOrDefault(w => w.ID == order.ID);
+
+                var beginTime = workOrderWithInclude.MalfunctionInfo.BeginTime;
+                var endTime = workOrderWithInclude.Repair.EndTime;
+
+                if (endTime != null)
+                {
+                    totalTime = (endTime.GetValueOrDefault() - beginTime).TotalHours;
+                }
+            }
+
+            return Math.Round(totalTime, 1);
+        }
+    }
+}
