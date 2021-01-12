db87a6cedd6b17a1398589f1180e69ecde77ffed
马扬杰 <ma-yangjie@outlook.com>
2021-01-12 17:41:12 +0800

    增加故障工单各流程的权限控制

---
+++ EquipmentManagementSystem/Authorization/Malfunction/AccessoriesOrderAuthorizationHandler.cs
@@ -0,0 +1,43 @@
+﻿using EquipmentManagementSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EquipmentManagementSystem.Authorization.Malfunction
+{
+    public class AccessoriesOrderAuthorizationHandler
+        : AuthorizationHandler<OperationAuthorizationRequirement, AccessoriesOrder>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+                                                        OperationAuthorizationRequirement requirement,
+                                                        AccessoriesOrder resource)
+        {
+            if (context.User == null || resource == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (requirement.Name == Constants.UpdateOperationName)
+            {
+                if (context.User.IsInRole(Constants.DirectorRole) || context.User.IsInRole(Constants.ManagerRole))
+                {
+                    context.Succeed(requirement);
+                }
+                else if (context.User.IsInRole(Constants.PrincipalRole) && 
+                    resource.MalfunctionWorkOrder.Instrument.Group == context.User.FindFirst("Group")?.Value)
+                {
+                    context.Succeed(requirement);
+                }
+                //else if (context.User.IsInRole(Constants.TechnicianRole) &&
+                //    resource.MalfunctionWorkOrder.Instrument.Group == context.User.FindFirst("Group")?.Value &&
+                //    resource.MalfunctionWorkOrder.Creator == context.User.FindFirst(ClaimTypes.GivenName)?.Value)
+                //{
+                //    context.Succeed(requirement);
+                //}
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
