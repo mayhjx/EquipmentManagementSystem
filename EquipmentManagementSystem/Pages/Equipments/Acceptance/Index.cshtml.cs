1d62c75eb39883f6b81b333eae1a073a1375838e
马扬杰 <ma-yangjie@outlook.com>
2021-01-12 15:11:39 +0800

    增加权限控制语句

--- EquipmentManagementSystem/Pages/Equipments/Acceptance/Index.cshtml.cs
+++ EquipmentManagementSystem/Pages/Equipments/Acceptance/Index.cshtml.cs
@@ -2,27 +2,42 @@
 using System.Threading.Tasks;
 using EquipmentManagementSystem.Data;
 using EquipmentManagementSystem.Models;
+using EquipmentManagementSystem.Pages.Instruments;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using EquipmentManagementSystem.Authorization;
 
 namespace EquipmentManagementSystem.Pages.Equipments.Acceptance
 {
-    public class IndexModel : PageModel
+    public class IndexModel : BasePageModel
     {
-        private readonly EquipmentContext _context;
-
-        public IndexModel(EquipmentContext context)
+        public IndexModel(EquipmentContext context,
+            UserManager<User> userManager,
+            IAuthorizationService authorizationService)
+            : base(context, userManager, authorizationService)
         {
-            _context = context;
         }
 
         public IList<InstrumentAcceptance> InstrumentAcceptance { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
             InstrumentAcceptance = await _context.InstrumentAcceptances
                                             .AsNoTracking()
                                             .ToListAsync();
+
+            var isAuthorized = await _authorizationService.AuthorizeAsync(User, InstrumentAcceptance.FirstOrDefault(), Operations.Read);
+
+            if (!isAuthorized.Succeeded)
+            {
+                return Forbid();
+            }
+
+            return Page();
         }
     }
 }
