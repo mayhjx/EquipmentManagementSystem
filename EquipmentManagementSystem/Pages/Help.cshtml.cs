85a11bfb7e8373e684a33ecb21af5f3f91b69769
马扬杰 <ma-yangjie@outlook.com>
2021-01-15 09:19:32 +0800

    增加帮助

---
+++ EquipmentManagementSystem/Pages/Help.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace EquipmentManagementSystem.Pages
+{
+    [AllowAnonymous]
+    public class HelpModel : PageModel
+    {
+        private readonly ILogger<HelpModel> _logger;
+
+        public HelpModel(ILogger<HelpModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
