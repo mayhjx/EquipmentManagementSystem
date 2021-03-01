08997e2adbbea468b7831d238aaa54b0a96c019f
马扬杰 <ma-yangjie@outlook.com>
2021-03-01 11:48:59 +0800

    Add YuanSu UsageRecord Repository

---
+++ EquipmentManagementSystem/Interfaces/IUsageRecordOfYuanSuRepository.cs
@@ -0,0 +1,13 @@
+﻿using EquipmentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EquipmentManagementSystem.Interfaces
+{
+    public interface IUsageRecordOfYuanSuRepository : IGenericRepository<UsageRecordOfYuanSu>
+    {
+        List<UsageRecordOfYuanSu> GetAllByInstrumentIdAndMonthOfBeginTime(string instrumentId, DateTime date);
+        double GetTotalUsageHoursOfRecords(List<UsageRecordOfYuanSu> usageRecords);
+    }
+}
---
+++ EquipmentManagementSystem/Repositories/UsageRecordOfYuanSuRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EquipmentManagementSystem.Interfaces;
+using EquipmentManagementSystem.Models;
+using EquipmentManagementSystem.Data;
+
+namespace EquipmentManagementSystem.Repositories
+{
+    public class UsageRecordOfYuanSuRepository : GenericRepository<UsageRecordOfYuanSu>, IUsageRecordOfYuanSuRepository
+    {
+        public UsageRecordOfYuanSuRepository(EquipmentContext context):base(context)
+        {
+        }
+
+        public List<UsageRecordOfYuanSu> GetAllByInstrumentIdAndMonthOfBeginTime(string instrumentId, DateTime date)
+        {
+            return _context.Set<UsageRecordOfYuanSu>()
+                .AsEnumerable()
+                .Where(i => i.InstrumentID == instrumentId)
+                .Where(i => i.BeginTime.GetValueOrDefault().Year == date.Year)
+                .Where(i => i.BeginTime.GetValueOrDefault().Month == date.Month)
+                .OrderBy(i => i.BeginTime)
+                .ToList();
+        }
+
+        public double GetTotalUsageHoursOfRecords(List<UsageRecordOfYuanSu> usageRecords)
+        {
+            double total = 0;
+            foreach (var record in usageRecords)
+            {
+                var beginTime = record.BeginTime;
+                var endTime = record.EndTime;
+
+                if (beginTime != null && endTime != null)
+                {
+                    total += (endTime - beginTime).GetValueOrDefault().TotalHours;
+                }
+            }
+            return Math.Round(total, 1);
+        }
+    }
+}
