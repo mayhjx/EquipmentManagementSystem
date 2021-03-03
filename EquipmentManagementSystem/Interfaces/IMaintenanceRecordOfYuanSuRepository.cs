1283f59c1aa2fda7f02d2978291193f74ae22b28
马扬杰 <ma-yangjie@outlook.com>
2021-03-03 17:48:57 +0800

    增加元素维护登记Repository

---
+++ EquipmentManagementSystem/Interfaces/IMaintenanceRecordOfYuanSuRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EquipmentManagementSystem.Models;
+
+namespace EquipmentManagementSystem.Interfaces
+{
+    public interface IMaintenanceRecordOfYuanSuRepository : IGenericRepository<MaintenanceRecordOfYuanSu>
+    {
+        Task CreateRecords(List<MaintenanceRecordOfYuanSu> maintenanceRecords);
+        List<MaintenanceRecordOfYuanSu> GetAllByInstrumentIdAndYearAndMonth(string instrumentId, DateTime? date);
+
+        // 待维护提醒 TODO
+    }
+}
---
+++ EquipmentManagementSystem/Repositories/MaintenanceRecordOfYuanSuRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EquipmentManagementSystem.Interfaces;
+using EquipmentManagementSystem.Models;
+using EquipmentManagementSystem.Data;
+
+namespace EquipmentManagementSystem.Repositories
+{
+    public class MaintenanceRecordOfYuanSuRepository : GenericRepository<MaintenanceRecordOfYuanSu>, IMaintenanceRecordOfYuanSuRepository
+    {
+        public MaintenanceRecordOfYuanSuRepository(EquipmentContext context) : base(context)
+        {
+        }
+
+        public async Task CreateRecords(List<MaintenanceRecordOfYuanSu> maintenanceRecords)
+        {
+            await _context.Set<MaintenanceRecordOfYuanSu>().AddRangeAsync(maintenanceRecords);
+            await _context.SaveChangesAsync();
+        }
+
+        public List<MaintenanceRecordOfYuanSu> GetAllByInstrumentIdAndYearAndMonth(string instrumentId, DateTime? date)
+        {
+            if (date.GetValueOrDefault() == null)
+            {
+                date = DateTime.Now;
+            }
+
+            return _context.Set<MaintenanceRecordOfYuanSu>()
+                .AsEnumerable()
+                .Where(i => i.InstrumentID == instrumentId)
+                .Where(i => i.BeginTime.GetValueOrDefault().Year == date.GetValueOrDefault().Year)
+                .Where(i => i.BeginTime.GetValueOrDefault().Month == date.GetValueOrDefault().Month)
+                .ToList();
+        }
+    }
+}
