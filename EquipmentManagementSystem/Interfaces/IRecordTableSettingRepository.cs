32b5e253b2641385251d498680898767cb1582eb
马扬杰 <ma-yangjie@outlook.com>
2021-03-12 14:35:04 +0800

    新建RecordTableSetting Repository

---
+++ EquipmentManagementSystem/Interfaces/IRecordTableSettingRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EquipmentManagementSystem.Models;
+
+namespace EquipmentManagementSystem.Interfaces
+{
+    public interface IRecordTableSettingRepository:IGenericRepository<RecordTableSetting>
+    {
+        public string GetUsageRecordChineseTitle(string instrumentId);
+        public string GetUsageRecordEnglishTitle(string instrumentId);
+        public string GetUsageRecordTableNumber(string instrumentId);
+
+        public string GetMaintenanceRecordChineseTitle(string instrumentId);
+        public string GetMaintenanceRecordEnglishTitle(string instrumentId);
+        public string GetMaintenanceRecordTableNumber(string instrumentId);
+    }
+}
---
+++ EquipmentManagementSystem/Repositories/RecordTableSettingRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EquipmentManagementSystem.Data;
+using EquipmentManagementSystem.Models;
+using EquipmentManagementSystem.Interfaces;
+
+namespace EquipmentManagementSystem.Repositories
+{
+    public class RecordTableSettingRepository : GenericRepository<RecordTableSetting>, IRecordTableSettingRepository
+    {
+        public RecordTableSettingRepository(EquipmentContext context):base(context)
+        {
+        }
+
+        public string GetMaintenanceRecordChineseTitle(string instrumentId)
+        {
+            return Find(i => i.InstrumentID == instrumentId).FirstOrDefault()?.MaintenanceRecordTableChineseTitle;
+        }
+
+        public string GetMaintenanceRecordEnglishTitle(string instrumentId)
+        {
+            return Find(i => i.InstrumentID == instrumentId).FirstOrDefault()?.MaintenanceRecordTableEnglishTitle;
+        }
+
+        public string GetMaintenanceRecordTableNumber(string instrumentId)
+        {
+            return Find(i => i.InstrumentID == instrumentId).FirstOrDefault()?.MaintenanceRecordTableNumber;
+        }
+
+        public string GetUsageRecordChineseTitle(string instrumentId)
+        {
+            return Find(i => i.InstrumentID == instrumentId).FirstOrDefault()?.UsageRecordTableChineseTitle;
+        }
+
+        public string GetUsageRecordEnglishTitle(string instrumentId)
+        {
+            return Find(i => i.InstrumentID == instrumentId).FirstOrDefault()?.UsageRecordTableEnglishTitle;
+        }
+
+        public string GetUsageRecordTableNumber(string instrumentId)
+        {
+            return Find(i => i.InstrumentID == instrumentId).FirstOrDefault()?.UsageRecordTableNumber;
+        }
+    }
+}
